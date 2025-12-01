115cf9f2ca41c204cfb47a6e3591fa48010932f7
Guilherme Kato <guilhermekato@alunos.utfpr.edu.br>
2025-12-01 19:02:43 -0300

    feat: add Tailwind CSS configuration and initial HTML template for client management interface

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./templates/**/*.html"],
+    theme: {
+        extend: {
+            colors: {
+                roxo: "#422ed2",
+                bg1: "#314158",
+                bg2: "#1d2a3d",
+                navbar: "#0f172a",
+                bgborder: "#2e3641",
+            },
+        },
+    },
+    plugins: [],
+};
